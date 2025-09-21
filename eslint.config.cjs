f337e18eb0c5a5d39a263ee4004bd0f5520d2ef1
xGGOD_ <rblo2002@gmail.com>
2025-09-21 15:25:23 +0200

    9b3

---
+++ eslint.config.cjs
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    ignores: ['node_modules', 'dist'],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin')
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      'semi': ['error', 'always'],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      '@typescript-eslint/restrict-plus-operands': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-case-declarations': 'off'
+    }
+  }
+];
--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
-
-
-const express = require('express');
+import express from 'express';
+import { calculator, Operation } from './calculator';
 const app = express();
 
-app.get('/ping', (req, res) => {
+app.get('/ping', (_req, res) => {
   res.send('pong');
+});
+
+app.post('/calculate', (req, res) => {
+  const { value1, value2, op } = req.body;
+
+  const operation = op as Operation;
+
+  const result = calculator(Number(value1), Number(value2), operation);
+  res.send({ result });
 });
 
 const PORT = 3003;
