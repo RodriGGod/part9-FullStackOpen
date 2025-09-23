6869aab051d697800e5ad5e5b1aea1d3377fdf0a
RodriGGod <rblo2002@gmail.com>
2025-09-23 16:21:42 +0200

    9d finished

--- tsreact/src/App.tsx
+++ tsreact/src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { Note } from "./types";
+import { getAllNotes, createNote } from './noteService';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+export type NewNote = Omit<Note, "id">;
+
+const App = () => {
+  const [notes, setNotes] = useState<Note[]>([{ id: 1, content: "testing" }]);
+  const [newNote, setNewNote] = useState("");
+
+  useEffect(() => {
+    axios.get<Note[]>("http://localhost:3001/notes").then((response) => {
+      setNotes(response.data as Note[]);
+    });
+  }, []);
+
+  const noteCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    axios
+      .post<Note>("http://localhost:3001/notes", { content: newNote })
+      .then((response) => {
+        setNotes(notes.concat(response.data));
+      });
+
+    setNewNote("");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <form onSubmit={noteCreation}>
+        <input
+          value={newNote}
+          onChange={(event) => setNewNote(event.target.value)}
+        />
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>{note.content}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;
